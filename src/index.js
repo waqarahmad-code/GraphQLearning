93b4c9a571605a81e5fc05103f6da2c4ceab5740
waqarahmad-code <waqarahmad.nxt@gmail.com>
2022-02-08 15:55:01 +0500

    added new features in demo app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 import reportWebVitals from './reportWebVitals';
 import { ApolloClient, InMemoryCache, ApolloProvider,useQuery,gql } from '@apollo/client';
 import { render } from '@testing-library/react';
+import { NetworkStatus } from '@apollo/client';
 
 
 
-
-const client=new ApolloClient({
-uri:"https://48p1r2roz4.sse.codesandbox.io",
-cache:new InMemoryCache()
+const client = new ApolloClient({
+  uri: "https://71z1g.sse.codesandbox.io/",
+  cache: new InMemoryCache()
 });
 
 
@@ -24,6 +24,77 @@ query GetExchangeRates{
   }
 }
 `;
+
+
+const GET_DOGS=gql`
+query GetDogs{
+  dogs{
+    id
+    breed
+  }
+}
+`;
+
+
+
+
+function Dogs({ onDogSelected }) {
+  const { loading, error, data } = useQuery(GET_DOGS);
+
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
+
+  return (
+    <select name="dog" onChange={onDogSelected}>
+      {data.dogs.map(dog => (
+        <option key={dog.id} value={dog.breed}>
+          {dog.breed}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+
+const GET_DOG_PHOTO = gql`
+query Dog($breed:String!){
+  dog(breed:$breed){
+    id
+    displayImage
+  }
+}
+`;
+
+
+
+
+function DogPhoto({ breed }) {
+  const { loading, error, data, refetch, networkStatus } = useQuery(
+    GET_DOG_PHOTO,
+    {
+      variables: { breed },
+      notifyOnNetworkStatusChange: true
+      // pollInterval: 500
+    }
+  );
+
+  if (networkStatus === 4) return <p>Refetching!</p>;
+  if (loading) return null;
+  if (error) return `Error!: ${error}`;
+
+  return (
+    <div>
+      <div>
+        <img src={data.dog.displayImage} style={{ height: 100, width: 100 }} />
+      </div>
+      <button onClick={() => refetch()}>Refetch!</button>
+    </div>
+  );
+}
+
+
+
+
 
 
 function ExchangeRates(){
@@ -44,19 +115,27 @@ function ExchangeRates(){
 
 
 function App() {
-  return (
-    <div>
-      <h2>My first Apollo app 🚀</h2>
-      <ExchangeRates/>
-    </div>
+  
+  const [selectedDog,setSelectedDog]=useState(null);
+  function onDogSelected({target}){
+    setSelectedDog(target.value);
+  }
+
+  return(
+
+    <ApolloProvider client={client}>
+      <div>
+        <h2>Building Query components 🚀</h2>
+        {selectedDog && <DogPhoto breed={selectedDog} />}
+        <Dogs onDogSelected={onDogSelected} />
+      </div>
+    </ApolloProvider>
+
   );
+
+
 }
 
-render(
-  <ApolloProvider client={client}>
-    <App/>
-  </ApolloProvider>,
-  document.getElementById("root")
-);
+render(<App />, document.getElementById("root"));
 
 
